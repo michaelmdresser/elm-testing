e21a51fc049a7780408bce09cb12ded254846aac
Michael Dresser <michaelmdresser@gmail.com>
2016-12-03 13:06:37 -0700

    indentation to 4 spaces and NewGif functions

--- gifFetcher.elm
+++ gifFetcher.elm
@@ -1,32 +1,40 @@
 -- MODEL
 
 type alias Model =
-  { topic : String
-  , gifUrl : String
-  }
+    { topic : String
+    , gifUrl : String
+    }
 
 init : (Model, Cmd Msg)
 init =
-  (Model "cats" "waiting.gif", Cmd.none)
+    (Model "cats" "waiting.gif", Cmd.none)
 
 
 -- UPDATE
 
-type Msg = MorePlease
+type Msg
+    = MorePlease
+    | NewGif (Result, Http.Error String)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-  case msg of
-    MorePlease ->
-      (model, Cmd.none)
+    case msg of
+        MorePlease ->
+            (model, getRandomGif model.topic)
+
+        NewGif (Ok, newUrl) ->
+            ( { model | gifUrl = newUrl }, Cmd.none)
+
+        NewGif (Err _) ->
+            (model, Cmd.none)
 
 
 -- VIEW
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ h2 [] [text model.topic]
-    , img [src model.gifUrl] []
-    , button [ onClick MorePlease ] [ text "More Please!" ]
-    ]
+    div []
+        [ h2 [] [text model.topic]
+        , img [src model.gifUrl] []
+        , button [ onClick MorePlease ] [ text "More Please!" ]
+        ]
