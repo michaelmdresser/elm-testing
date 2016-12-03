ef7f40772b204796b8b5151a34c35362baae7497
Michael Dresser <michaelmdresser@gmail.com>
2016-12-03 12:59:19 -0700

    skeleton actual

---
+++ gifFetcher.elm
@@ -0,0 +1,32 @@
+-- MODEL
+
+type alias Model =
+  { topic : String
+  , gifUrl : String
+  }
+
+init : (Model, Cmd Msg)
+init =
+  (Model "cats" "waiting.gif", Cmd.none)
+
+
+-- UPDATE
+
+type Msg = MorePlease
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease ->
+      (model, Cmd.none)
+
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h2 [] [text model.topic]
+    , img [src model.gifUrl] []
+    , button [ onClick MorePlease ] [ text "More Please!" ]
+    ]
