f6a89620a474c1e433460cf515db5d0a4c6affe5
Michael Dresser <michaelmdresser@gmail.com>
2016-12-03 14:07:29 -0700

    progres on modifying shelves

--- pluralize.elm
+++ pluralize.elm
@@ -2,14 +2,30 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
+type alias Model =
+    { shelves : Int}
+model =
+    { shelves = 0 }
+
+
+update action model =
+    model
+
+
 pluralize singular plural quantity =
     if quantity == 1 then
         singular
     else
         plural
 
+
 main =
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+
+view : Model -> Html
+view address model =
     div [ class "content" ]
     [ h1 [] [ text "Pluralizer" ]
-    , text (pluralize "shelf" "shelves" 5)
+    , text (pluralize "shelf" "shelves" model.shelves)
     ]
