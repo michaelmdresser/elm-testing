f4f151e85f7dfa5946a1eb70002ffdeb492a0b83
Michael Dresser <michaelmdresser@gmail.com>
2016-12-03 19:03:36 -0700

    gifFetcher now uses example code from docs

--- gifFetcher.elm
+++ gifFetcher.elm
@@ -1,43 +1,84 @@
--- MODEL
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
 
-type alias Model =
-    { topic : String
-    , gifUrl : String
+
+
+main =
+  Html.program
+    { init = init "cats"
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
     }
 
-init : (Model, Cmd Msg)
-init =
-    (Model "cats" "waiting.gif", Cmd.none)
+
+
+-- MODEL
+
+
+type alias Model =
+  { topic : String
+  , gifUrl : String
+  }
+
+
+init : String -> (Model, Cmd Msg)
+init topic =
+  ( Model topic "waiting.gif"
+  , getRandomGif topic
+  )
+
 
 
 -- UPDATE
 
+
 type Msg
-    = MorePlease
-    | NewGif (Result, Http.Error String)
+  = MorePlease
+  | NewGif (Result Http.Error String)
+
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-        MorePlease ->
-            (model, getRandomGif model.topic)
+  case msg of
+    MorePlease ->
+      (model, getRandomGif model.topic)
 
-        NewGif (Ok newUrl) ->
-            ( { model | gifUrl = newUrl }, Cmd.none)
+    NewGif (Ok newUrl) ->
+      (Model model.topic newUrl, Cmd.none)
 
-        NewGif (Err _) ->
-            (model, Cmd.none)
+    NewGif (Err _) ->
+      (model, Cmd.none)
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-    div []
-        [ h2 [] [text model.topic]
-        , img [src model.gifUrl] []
-        , button [ onClick MorePlease ] [ text "More Please!" ]
-        ]
+  div []
+    [ h2 [] [text model.topic]
+    , button [ onClick MorePlease ] [ text "More Please!" ]
+    , br [] []
+    , img [src model.gifUrl] []
+    ]
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- HTTP
 
 
 getRandomGif : String -> Cmd Msg
@@ -45,12 +86,10 @@ getRandomGif topic =
   let
     url =
       "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
-
-    request =
-      Http.get url decodeGifUrl
   in
-    Http.send NewGif request
+    Http.send NewGif (Http.get url decodeGifUrl)
 
-decodeGifUrl : Json.Decoder String
+
+decodeGifUrl : Decode.Decoder String
 decodeGifUrl =
-  Json.at ["data", "image_url"] Json.string
+  Decode.at ["data", "image_url"] Decode.string
